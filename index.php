874d0d726142e0c9e15ee6ecb118a60e31abd375
Nicocahya <nr016082@gmail.com>
2024-02-01 15:40:44 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui, suscipit ipsam? Unde, quisquam! Cum quia aut ex consequatur maiores eveniet quos vitae placeat ratione libero eos, distinctio, ea quasi voluptate.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
